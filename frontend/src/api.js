9345c74226e0677873e8d7a0ec66f88d65f0574c
unknown <mugishajanvier12-dev>
2025-07-29 11:27:37 +0200

    update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -7,7 +7,7 @@ const api = axios.create({
 
 api.interceptors.request.use(
     (config)=>{
-        const token = localStorage.getItem('musha_front_token');
+        const token = localStorage.getItem('front_token');
         if(token){
             config.headers['Authorization'] = `Bearer ${token}`;
         }
